6dc15cc3a65f8f04b619c93db3288434aa77fc8f
Jose Figueroa <josemfigueroa04@gmail.com>
2023-06-30 19:11:14 -0400

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
-import Header from "./components/Header";
-import Formulario from "./components/Formulario";
-import ListadoPacientes from "./components/ListadoPacientes";
-import { useState } from "react";
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
   const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  }, [pacientes])
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados)
+  }
 
   return (
-    <div >
+    <div className="container mx-auto mt-20">
       <Header />
-      <div className="mt-12 flex md:">
-        <Formulario 
-          pacientes={pacientes}
-          setPacientes={setPacientes}
-        />
-        <ListadoPacientes 
-          pacientes={pacientes}
-        
-        />
-        
-       
-        
+
+      <div className="mt-12 md:flex">
+          <Formulario 
+            pacientes={pacientes}
+            setPacientes={setPacientes}
+            paciente={paciente}
+            setPaciente={setPaciente}
+          />
+          <ListadoPacientes 
+            pacientes={pacientes}
+            setPaciente={setPaciente}
+            eliminarPaciente={eliminarPaciente}
+          />
       </div>
+
     </div>
   )
 }
